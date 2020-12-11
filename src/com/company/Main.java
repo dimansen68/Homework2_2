c5639ee24f46f249cbac4f7397686ee873c7607f
dimansen68 <dimansen15@inbox.ru>
2020-12-11 16:32:06 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        double gross = 100000;
+        double net = gross*(1-0.13);
+        System.out.println("Зарплата " + net);
+    }
+}
